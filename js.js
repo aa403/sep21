daa6271b915c510592cb58c8d0d654a004e52948
Ammar <ammar.akhtar@icloud.com>
2014-07-14 20:57:35 +0100

    paralaX

--- js.js
+++ js.js
@@ -15,3 +15,15 @@ $('#showNavBar').on('click', function(){
 $('#navBar').on('click', function(){
 	hideNavBar($(this))
 });
+
+var plaxItems = $(document).find('.plaxItem');
+var z_index = 0;
+// var pos_top = 40;
+plaxItems.each(function(){
+	// pos_top = pos_top+40;
+	// $(this).css('top', pos_top);
+	$(this).css('z-index',++z_index);
+});
+$.stellar({
+	 verticalOffset: 150
+	});
