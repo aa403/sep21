21501b6a61f3229fdbb5d44dfb4b68962ebb70cd
aa403 <ammar.akhtar@icloud.com>
2014-08-20 00:50:45 +0100

    updates@

--- pw.js
+++ pw.js
@@ -1,6 +1,8 @@
 $(document).ready(function () { 
 	// back_target = "home"
-	$('#pw_attempt').focus();
+	if($('#pw_attempt').is("visible")){
+		$('#pw_attempt').focus();
+	}
 	$('html, body').scrollTop(0);
 })
 
@@ -49,3 +51,66 @@ $("#submit").on('click', function() {
     }); // Ajax Call
 
 })
+
+function getPageName(url) {
+    var index = url.lastIndexOf("/") + 1;
+    var filenameWithExtension = url.substr(index);
+    var filename = filenameWithExtension.split(".")[0]; // <-- added this line
+    return filename;                                    // <-- added this line
+}
+
+
+// on click, do these things:
+$(".navItem").live('click', function() {
+	target=$(this).data("target")
+	// remove currentPage class from all navItems
+    $(".currentPage").removeClass("currentPage")
+	// add currentPage class to $this
+    $(this).addClass("currentPage")
+	
+	// find all divs with classes displayedContent
+	back_target = $(".displayedContent").attr('id')
+	$(".displayedContent").addClass("hiddenContent")
+	$(".displayedContent").removeClass("displayedContent")
+	// $('html, body').animate({ scrollTop: 0 }, 'slow');
+
+	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
+	target_ele = $("#".concat(target))
+	target_ele.removeClass('hiddenContent')
+	target_ele.addClass('displayedContent')
+
+});
+
+
+$(".nav").live('click', function() {
+	target=$(this).data("target")
+	// remove currentPage class from all navItems
+    $(".currentPage").removeClass("currentPage")
+    
+	// set nav_ele as the navItem with data-target = target
+	nav_ele = $(".navItem[data-target=".concat(target).concat("]"))
+	
+
+	// add currentPage class to nav_ele
+    $(nav_ele).addClass("currentPage")
+	
+	// find all divs with classes displayedContent
+	$(".displayedContent").addClass("hiddenContent")
+	$(".displayedContent").removeClass("displayedContent")
+	// $('html, body').animate({ scrollTop: 0 }, 'slow');
+
+	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
+	target_ele = $("#".concat(target))
+	target_ele.removeClass('hiddenContent')
+	target_ele.addClass('displayedContent')
+
+});
+
+
+// open external link
+$('.to-link').live('click',function() {
+	target=$(this).data("target")
+	window.open(target);
+  });
+
+
