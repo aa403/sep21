09cbc1829962c80075b73fadca72f7f0bc3226c8
aa403 <ammar.akhtar@icloud.com>
2014-08-20 01:00:15 +0100

    updates@

--- js.js
+++ js.js
@@ -1,64 +1,65 @@
-// $(document).ready(function () { 
-// 	$('html, body').scrollTop(0);
-// })
+$(document).ready(function () { 
+	$('html, body').scrollTop(0);
+})
 
-// function getPageName(url) {
-//     var index = url.lastIndexOf("/") + 1;
-//     var filenameWithExtension = url.substr(index);
-//     var filename = filenameWithExtension.split(".")[0]; // <-- added this line
-//     return filename;                                    // <-- added this line
-// }
+function getPageName(url) {
+    var index = url.lastIndexOf("/") + 1;
+    var filenameWithExtension = url.substr(index);
+    var filename = filenameWithExtension.split(".")[0]; // <-- added this line
+    return filename;                                    // <-- added this line
+}
 
 
-// // on click, do these things:
-// $(".navItem").on('click', function() {
-// 	target=$(this).data("target")
-// 	// remove currentPage class from all navItems
-//     $(".currentPage").removeClass("currentPage")
-// 	// add currentPage class to $this
-//     $(this).addClass("currentPage")
+// on click, do these things:
+$(".navItem").on('click', function() {
+	target=$(this).data("target")
+	// remove currentPage class from all navItems
+    $(".currentPage").removeClass("currentPage")
+	// add currentPage class to $this
+    $(this).addClass("currentPage")
 	
-// 	// find all divs with classes displayedContent
-// 	back_target = $(".displayedContent").attr('id')
-// 	$(".displayedContent").addClass("hiddenContent")
-// 	$(".displayedContent").removeClass("displayedContent")
-// 	// $('html, body').animate({ scrollTop: 0 }, 'slow');
+	// find all divs with classes displayedContent
+	$(".displayedContent").addClass("hiddenContent")
+	$(".displayedContent").removeClass("displayedContent")
+	// $('html, body').animate({ scrollTop: 0 }, 'slow');
 
-// 	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
-// 	target_ele = $("#".concat(target))
-// 	target_ele.removeClass('hiddenContent')
-// 	target_ele.addClass('displayedContent')
+	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
+	target_ele = $("#".concat(target))
+	target_ele.removeClass('hiddenContent')
+	target_ele.addClass('displayedContent')
+	$('html, body').scrollTop(0);
 
-// });
+});
 
 
-// $(".nav").on('click', function() {
-// 	target=$(this).data("target")
-// 	// remove currentPage class from all navItems
-//     $(".currentPage").removeClass("currentPage")
+$(".nav").on('click', function() {
+	target=$(this).data("target")
+	// remove currentPage class from all navItems
+    $(".currentPage").removeClass("currentPage")
     
-// 	// set nav_ele as the navItem with data-target = target
-// 	nav_ele = $(".navItem[data-target=".concat(target).concat("]"))
+	// set nav_ele as the navItem with data-target = target
+	nav_ele = $(".navItem[data-target=".concat(target).concat("]"))
 	
 
-// 	// add currentPage class to nav_ele
-//     $(nav_ele).addClass("currentPage")
+	// add currentPage class to nav_ele
+    $(nav_ele).addClass("currentPage")
 	
-// 	// find all divs with classes displayedContent
-// 	$(".displayedContent").addClass("hiddenContent")
-// 	$(".displayedContent").removeClass("displayedContent")
-// 	// $('html, body').animate({ scrollTop: 0 }, 'slow');
+	// find all divs with classes displayedContent
+	$(".displayedContent").addClass("hiddenContent")
+	$(".displayedContent").removeClass("displayedContent")
+	// $('html, body').animate({ scrollTop: 0 }, 'slow');
 
-// 	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
-// 	target_ele = $("#".concat(target))
-// 	target_ele.removeClass('hiddenContent')
-// 	target_ele.addClass('displayedContent')
+	// find div with id == $this.data-target, remove class hiddenContent, add class displayedContent
+	target_ele = $("#".concat(target))
+	target_ele.removeClass('hiddenContent')
+	target_ele.addClass('displayedContent')
+	$('html, body').scrollTop(0);
 
-// });
+});
 
 
-// // open external link
-// $('.to-link').click(function() {
-// 	target=$(this).data("target")
-// 	window.open(target);
-//   });
+// open external link
+$('.to-link').on('click', function() {
+	target=$(this).data("target")
+	window.open(target);
+  });
