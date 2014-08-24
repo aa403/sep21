7ba2352404a3a9b992c8c366789d66c0c574f53f
aa403 <ammar.akhtar@icloud.com>
2014-08-24 10:43:43 +0100

    minor bugs

--- songs.php
+++ songs.php
@@ -4,21 +4,13 @@
 		// $song = "'".$_GET["q"];
 		// $song = $song."'";
 
-		$dbconn = pg_connect("host=ec2-54-247-118-153.eu-west-1.compute.amazonaws.com 
-					dbname=d7ae6vj91njv84
-					user=cwazygvwzuhpuh
-					password=hS_ZGtu5gu0dTJaCuhcwpvybmY")
+		$dbconn = pg_connect("host=ec2-54-228-227-236.eu-west-1.compute.amazonaws.com 
+					dbname=d76ka7eujlrtci
+					user=wcuvqmvohiekbv
+					password=sX7xoQepIEhS8PlXdfSz3Fexpb")
 				or die('Could not connect: ' . pg_last_error());
 
-		//perform the insert using pg_query
-		//$result = pg_query($dbconn, "INSERT INTO songs(request, created) VALUES(\"$song\", time());");
-				// ERROR:  column "sine" does not exist\nLINE 1:
-				// INSERT INTO songs(request)
-				// 			VALUES("sine");\n 
-				// INSERT INTO films (code, title, did, date_prod, kind)
-				// 			VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');
-		// $result = pg_query($dbconn, "INSERT INTO songs (request) VALUES($song)");
-		$result = pg_query_params($dbconn, "INSERT INTO songs (request) VALUES($1)", array($song));
+		$result = pg_query_params($dbconn, 'INSERT INTO songs (request) VALUES($1)', array($song));
 
 		//dump the result object
 		if ($result == false) {
