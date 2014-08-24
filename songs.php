9de788d48ecf84028b50761851a1c8a76a1bad42
aa403 <ammar.akhtar@icloud.com>
2014-08-24 02:19:41 +0100

    minor bugs

--- songs.php
+++ songs.php
@@ -1,6 +1,6 @@
 <?php
 	if ($_GET['q']){
-		$song = $_GET["q"];
+		$song = "'".$_GET["q"]."'";
 
 		$dbconn = pg_connect("host=ec2-54-247-118-153.eu-west-1.compute.amazonaws.com 
 					dbname=d7ae6vj91njv84
@@ -10,7 +10,12 @@
 
 		//perform the insert using pg_query
 		//$result = pg_query($dbconn, "INSERT INTO songs(request, created) VALUES(\"$song\", time());");
-		$result = pg_query($dbconn, "INSERT INTO songs(request) VALUES(\"$song\");");
+				// ERROR:  column "sine" does not exist\nLINE 1:
+				// INSERT INTO songs(request)
+				// 			VALUES("sine");\n 
+				// INSERT INTO films (code, title, did, date_prod, kind)
+				// 			VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');
+		$result = pg_query($dbconn, "INSERT INTO songs (request) VALUES($song)");
 
 		//dump the result object
 		if ($result == false) {
